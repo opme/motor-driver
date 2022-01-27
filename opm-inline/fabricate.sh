1b022e0892b3ad8e91284862eff24db7402bb1a1
Michael Shamberger <shambergerm@gmail.com>
2022-01-27 15:32:45 +0200

    update fab

--- opm-inline/fabricate.sh
+++ opm-inline/fabricate.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-kikit fab jlcpcb --no-drc --assembly --schematic source/opm-inline.sch --field LCSC panel.opm-inline.kicad_pcb build
+kikit fab jlcpcb --no-drc --assembly --schematic source/opm-inline.kicad_sch --field LCSC panel.opm-inline.kicad_pcb build
